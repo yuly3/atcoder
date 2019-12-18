2cb8d1675f63a75bf432564c9e700fcedc0e9644
yuly3masapon <yuly3masapon@gmail.com>
2019-12-18 23:08:06 +0900

    update

--- ABC145/D.py
+++ ABC145/D.py
@@ -1,3 +1,14 @@
+def cmb(n, r, mod):
+    if r < 0 or n < r:
+        return 0
+    r = min(r, n-r)
+    numerator, denominator = 1, 1
+    for i in range(1, r + 1):
+        numerator = (numerator * (n + 1 - i)) % mod
+        denominator = (denominator * i) % mod
+    return numerator * pow(denominator, mod - 2, mod) % mod
+
+
 def solve():
     MOD = 10**9+7
     X, Y = map(int, input().split())
@@ -5,8 +16,16 @@ def solve():
     if (X + Y) % 3 != 0:
         print(0)
         exit()
-
-
+    
+    n = (2 * Y - X) // 3
+    m = (X - n) // 2
+    
+    if n < 0 or m < 0:
+        print(0)
+        exit()
+    
+    ans = cmb(n + m, n, MOD)
+    print(ans)
 
 
 if __name__ == '__main__':
