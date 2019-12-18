f0400d6270ed3eb2ae9cd17d3c93ab4fb6dc9678
yuly3masapon <yuly3masapon@gmail.com>
2019-12-18 23:23:47 +0900

    update

---
+++ parts.py
@@ -0,0 +1,29 @@
+def cmb(n, r, mod):
+    if r < 0 or n < r:
+        return 0
+    r = min(r, n-r)
+    numerator, denominator = 1, 1
+    for i in range(1, r + 1):
+        numerator = (numerator * (n + 1 - i)) % mod
+        denominator = (denominator * i) % mod
+    return numerator * pow(denominator, mod - 2, mod) % mod
+
+
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp % i == 0:
+            cnt = 0
+            while temp % i == 0:
+                cnt += 1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp != 1:
+        arr.append([temp, 1])
+
+    if not arr:
+        arr.append([n, 1])
+
+    return arr
