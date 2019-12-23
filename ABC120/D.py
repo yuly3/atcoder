88bf2362236fb2930730e153e19a439c874109bb
yuly3 <yuly3masapon@gmail.com>
2019-12-23 14:21:38 +0900

    update

--- ABC120/D.py
+++ ABC120/D.py
@@ -17,11 +17,30 @@ def find_root(x):
 
 
 def merge(x, y):
-
+    rx = find_root(x)
+    ry = find_root(y)
+    if rx == ry:
+        return 0
+    sx = comp_size[rx]
+    sy = comp_size[ry]
+    if sy < sx:
+        root[ry] = rx
+        comp_size[rx] += sy
+    else:
+        root[rx] = ry
+        comp_size[ry] += sx
+    return sx * sy
 
 
 def solve():
+    disconnect = []
+    x = N * (N-1) // 2
+    for a, b in AB[::-1]:
+        disconnect.append(x)
+        x -= merge(a, b)
 
+    for ans in disconnect[::-1]:
+        print(ans)
 
 
 if __name__ == '__main__':
