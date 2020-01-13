975e3c2d521968c97af658c43f4774e0a746cbbf
yuly3masapon <yuly3masapon@gmail.com>
2020-01-13 12:45:40 +0900

    update

--- ABC151/D.nim
+++ ABC151/D.nim
@@ -4,18 +4,16 @@ proc solve() =
     # Nim ver1.1.1
     var H, W: int
     (H, W) = stdin.readLine.split.map(parseInt)
-    type P = object
-        y, x: int
     var
         maze = newSeqWith(H, repeat(-2, W))
-        s_point = initDeque[P]()
+        s_point = initDeque[(int, int)]()
     for i in 0..<H:
         var line = stdin.readLine
         for j in 0..<W:
             if line[j] == '#':
                 maze[i][j] = -1
             else:
-                s_point.addLast(P(y: i, x: j))
+                s_point.addLast((i, j))
     
     let dyx = [[-1, 0], [0, 1], [1, 0], [0, -1]]
     var
@@ -25,21 +23,21 @@ proc solve() =
         var
             ans_tmp = 0
             maze_tmp = deepCopy(maze)
-            start = s_point.popFirst
-            que = initDeque[P]()
-        que.addLast(P(y: start.y, x: start.x))
-        maze_tmp[start.y][start.x] = 0
+            (sy, sx) = s_point.popFirst
+            que = initDeque[(int, int)]()
+        que.addLast((sy, sx))
+        maze_tmp[sy][sx] = 0
         while que.len != 0:
-            let cur = que.popFirst
+            let (cy, cx) = que.popFirst
             for i in 0..3:
                 var
-                    ny = cur.y + dyx[i][0]
-                    nx = cur.x + dyx[i][1]
+                    ny = cy + dyx[i][0]
+                    nx = cx + dyx[i][1]
                 if 0 <= ny and ny < H and 0 <= nx and nx < W:
                     if maze_tmp[ny][nx] == -2:
-                        maze_tmp[ny][nx] = maze_tmp[cur.y][cur.x] + 1
+                        maze_tmp[ny][nx] = maze_tmp[cy][cx] + 1
                         ans_tmp = max(ans_tmp, maze_tmp[ny][nx])
-                        que.addLast(P(y: ny, x: nx))
+                        que.addLast((ny, nx))
         ans = max(ans, ans_tmp)
     echo ans
 
--- ABC151/D_old.nim
+++ ABC151/D_old.nim
@@ -4,18 +4,16 @@ proc solve() =
     # Nim ver0.13
     var H, W: int
     (H, W) = stdin.readLine.split.map(parseInt)
-    type P = object
-        y, x: int
     var
         maze = newSeqWith(H, repeat(-2, W))
-        s_point = initQueue[P]()
+        s_point = initQueue[(int, int)]()
     for i in 0..<H:
         var line = stdin.readLine
         for j in 0..<W:
             if line[j] == '#':
                 maze[i][j] = -1
             else:
-                s_point.enqueue(P(y: i, x: j))
+                s_point.enqueue((i, j))
     
     let dyx = [[-1, 0], [0, 1], [1, 0], [0, -1]]
     var
@@ -25,21 +23,21 @@ proc solve() =
         var
             ans_tmp = 0
             maze_tmp = maze
-            start = s_point.dequeue
-            que = initQueue[P]()
-        que.enqueue(P(y: start.y, x: start.x))
-        maze_tmp[start.y][start.x] = 0
+            (sy, sx) = s_point.dequeue
+            que = initQueue[(int, int)]()
+        que.enqueue((sy, sx))
+        maze_tmp[sy][sx] = 0
         while que.len != 0:
-            let cur = que.dequeue
+            let (cy, cx) = que.dequeue
             for i in 0..3:
                 var
-                    ny = cur.y + dyx[i][0]
-                    nx = cur.x + dyx[i][1]
+                    ny = cy + dyx[i][0]
+                    nx = cx + dyx[i][1]
                 if 0 <= ny and ny < H and 0 <= nx and nx < W:
                     if maze_tmp[ny][nx] == -2:
-                        maze_tmp[ny][nx] = maze_tmp[cur.y][cur.x] + 1
+                        maze_tmp[ny][nx] = maze_tmp[cy][cx] + 1
                         ans_tmp = max(ans_tmp, maze_tmp[ny][nx])
-                        que.enqueue(P(y: ny, x: nx))
+                        que.enqueue((ny, nx))
         ans = max(ans, ans_tmp)
     echo ans
 
