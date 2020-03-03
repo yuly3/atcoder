7bb6b55223e2ee30ed60552e281e215962201592
yuly3masapon <yuly3masapon@gmail.com>
2020-03-03 17:35:30 +0900

    update

--- DISCO presents ディスカバリーチャンネル コードコンテスト2020 予選/D.py
+++ DISCO presents ディスカバリーチャンネル コードコンテスト2020 予選/D.py
@@ -1,10 +1,15 @@
 def solve():
-    m = int(input())
-    dc_l = {}
-    for _ in range(m):
-        line = list(map(int, input().split()))
-        dc_l[line[0]] = line[1]
-    ans = 0
+    M = int(input())
+    d, c = [0] * M, [0] * M
+    for i in range(M):
+        d[i], c[i] = map(int, input().split())
+    
+    sum_d = 0
+    for i in range(M):
+        sum_d += d[i] * c[i]
+    sum_c = sum(c)
+    ans = (sum_c - 1) + (sum_d - 1) // 9
+    print(ans)
 
 
 if __name__ == '__main__':
