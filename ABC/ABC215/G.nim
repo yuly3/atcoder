ddd8444b2c810e20e84493765a8b481d2715350f
yuly3masapon <yuly3masapon@gmail.com>
2021-08-22 13:55:47 +0900

    update

---
+++ ABC/ABC215/G.nim
@@ -0,0 +1,177 @@
+when not declared ATCODER_YULY3HEADER_HPP:
+  const ATCODER_YULY3HEADER_HPP* = 1
+
+  import
+    algorithm,
+    bitops,
+    deques,
+    heapqueue,
+    math,
+    macros,
+    sets,
+    sequtils,
+    strformat,
+    strutils,
+    sugar,
+    tables
+
+  proc transLastStmt(n, res, bracketExpr: NimNode): (NimNode, NimNode, NimNode) =
+    # Looks for the last statement of the last statement, etc...
+    case n.kind
+    of nnkIfExpr, nnkIfStmt, nnkTryStmt, nnkCaseStmt:
+      result[0] = copyNimTree(n)
+      result[1] = copyNimTree(n)
+      result[2] = copyNimTree(n)
+      for i in ord(n.kind == nnkCaseStmt)..<n.len:
+        (result[0][i], result[1][^1], result[2][^1]) = transLastStmt(n[i], res, bracketExpr)
+    of nnkStmtList, nnkStmtListExpr, nnkBlockStmt, nnkBlockExpr, nnkWhileStmt,
+        nnkForStmt, nnkElifBranch, nnkElse, nnkElifExpr, nnkOfBranch, nnkExceptBranch:
+      result[0] = copyNimTree(n)
+      result[1] = copyNimTree(n)
+      result[2] = copyNimTree(n)
+      if n.len >= 1:
+        (result[0][^1], result[1][^1], result[2][^1]) = transLastStmt(n[^1], res, bracketExpr)
+    of nnkTableConstr:
+      result[1] = n[0][0]
+      result[2] = n[0][1]
+      if bracketExpr.len == 1:
+        bracketExpr.add([newCall(bindSym"typeof", newEmptyNode()), newCall(
+            bindSym"typeof", newEmptyNode())])
+      template adder(res, k, v) = res[k] = v
+      result[0] = getAst(adder(res, n[0][0], n[0][1]))
+    of nnkCurly:
+      result[2] = n[0]
+      if bracketExpr.len == 1:
+        bracketExpr.add(newCall(bindSym"typeof", newEmptyNode()))
+      template adder(res, v) = res.incl(v)
+      result[0] = getAst(adder(res, n[0]))
+    else:
+      result[2] = n
+      if bracketExpr.len == 1:
+        bracketExpr.add(newCall(bindSym"typeof", newEmptyNode()))
+      template adder(res, v) = res.add(v)
+      result[0] = getAst(adder(res, n))
+
+  macro collect*(init, body: untyped): untyped =
+    runnableExamples:
+      import sets, tables
+      let data = @["bird", "word"]
+      ## seq:
+      let k = collect(newSeq):
+        for i, d in data.pairs:
+          if i mod 2 == 0: d
+      assert k == @["bird"]
+      ## seq with initialSize:
+      let x = collect(newSeqOfCap(4)):
+        for i, d in data.pairs:
+          if i mod 2 == 0: d
+      assert x == @["bird"]
+      ## HashSet:
+      let y = initHashSet.collect:
+        for d in data.items: {d}
+      assert y == data.toHashSet
+      ## Table:
+      let z = collect(initTable(2)):
+        for i, d in data.pairs: {i: d}
+      assert z == {0: "bird", 1: "word"}.toTable
+    
+    let res = genSym(nskVar, "collectResult")
+    expectKind init, {nnkCall, nnkIdent, nnkSym}
+    let bracketExpr = newTree(nnkBracketExpr,
+      if init.kind == nnkCall: init[0] else: init)
+    let (resBody, keyType, valueType) = transLastStmt(body, res, bracketExpr)
+    if bracketExpr.len == 3:
+      bracketExpr[1][1] = keyType
+      bracketExpr[2][1] = valueType
+    else:
+      bracketExpr[1][1] = valueType
+    let call = newTree(nnkCall, bracketExpr)
+    if init.kind == nnkCall:
+      for i in 1 ..< init.len:
+        call.add init[i]
+    result = newTree(nnkStmtListExpr, newVarStmt(res, call), resBody, res)
+
+  proc input*(): string {.inline.} = stdin.readLine
+  proc inputs*(): seq[string] {.inline.} = stdin.readLine.split
+  proc inputInt*(): int {.inline.} = stdin.readLine.parseInt
+  proc inputInts*(): seq[int] {.inline.} = stdin.readLine.split.map(parseInt)
+  proc chmax*[T: SomeNumber](n: var T, m: T) {.inline.} = n = max(n, m)
+  proc chmin*[T: SomeNumber](n: var T, m: T) {.inline.} = n = min(n, m)
+  proc `%=`*[T: SomeInteger](n: var T, m: T) {.inline.} = n = floorMod(n, m)
+  proc `|=`*[T: SomeInteger or bool](n: var T, m: T) {.inline.} = n = n or m
+  proc `&=`*[T: SomeInteger or bool](n: var T, m: T) {.inline.} = n = n and m
+  proc `^=`*[T: SomeInteger or bool](n: var T, m: T) {.inline.} = n = n xor m
+  proc `<<=`*[T: SomeInteger](n: var T, m: T) {.inline.} = n = n shl m
+  proc `>>=`*[T: SomeInteger](n: var T, m: T) {.inline.} = n = n shr m
+
+when not declared ATCODER_COMBINATION_HPP:
+  const ATCODER_COMBINATION_HPP* = 1
+
+  type
+    Combination* = ref object
+      MOD: int
+      fact, factInv, inv: seq[int]
+  
+  proc initCombination*(n: int, MOD=10^9 + 7): Combination =
+    var
+      fact = newSeq[int](n + 1)
+      factInv = newSeq[int](n + 1)
+      inv = newSeq[int](n + 1)
+    fact[0] = 1; fact[1] = 1
+    factInv[0] = 1; factInv[1] = 1
+    inv[1] = 1
+    for i in 2..n:
+      fact[i] = floorMod(fact[i - 1]*i, MOD)
+      inv[i] = floorMod(-inv[MOD mod i]*(MOD div i), MOD)
+      factInv[i] = floorMod(factInv[i - 1]*inv[i], MOD)
+    return Combination(MOD: MOD, fact: fact, factInv: factInv, inv: inv)
+
+  proc nCr*(self: var Combination, n, r: int): int =
+    if r < 0 or n < r:
+      return 0
+    let r = min(r, n - r)
+    return (self.fact[n]*self.factInv[r] mod self.MOD)*self.factInv[n - r] mod self.MOD
+
+  proc nHr*(self: var Combination, n, r: int): int =
+    return self.nCr(n + r - 1, r)
+
+  proc nPr*(self: var Combination, n, r: int): int =
+    if r < 0 or n < r:
+      return 0
+    return self.fact[n]*self.factInv[n - r] mod self.MOD
+
+proc modPow*(a, k: int, MOD=10^9 + 7): int =
+  var
+    a = a
+    k = k
+  result = 1
+  while k > 0:
+    if bitand(k, 1) == 1:
+      result = floorMod(result*a, MOD)
+    a = floorMod(a*a, MOD)
+    k = k shr 1
+
+proc modInv*(a: int, MOD=10^9 + 7): int =
+  return modPow(a, MOD - 2, MOD)
+
+when isMainModule:
+  const MOD = 998244353
+  var N = inputInt()
+  var c = inputInts()
+
+  var c2cnt = c.toCountTable()
+  var cnt2cnt: Table[int, int]
+  for (ci, cnt) in c2cnt.pairs():
+    cnt2cnt.mgetOrPut(cnt, 0).inc
+  
+  var cmb = initCombination(N, MOD)
+  var ans: seq[int]
+  for k in 1..N:
+    var exp: int
+    for (x, cnt) in cnt2cnt.pairs():
+      exp += cnt*(cmb.nCr(N, k) - cmb.nCr(N - x, k))
+    exp %= MOD
+    exp *= modInv(cmb.nCr(N, k), MOD)
+    exp %= MOD
+    ans.add(exp)
+  echo ans.join("\n")
