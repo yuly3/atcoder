13e27c2f27f2ca464dc6808718a9d14e3a425b70
yuly3masapon <yuly3masapon@gmail.com>
2021-10-10 09:34:58 +0900

    update

---
+++ other/joi2020ho/C.nim
@@ -0,0 +1,150 @@
+when not declared ATCODER_YULY3HEADER_HPP:
+  const ATCODER_YULY3HEADER_HPP* = 1
+  
+  import
+    algorithm,
+    bitops,
+    deques,
+    heapqueue,
+    math,
+    macros,
+    sets,
+    sequtils,
+    strformat,
+    strutils,
+    sugar,
+    tables
+  
+  proc transLastStmt(n, res, bracketExpr: NimNode): (NimNode, NimNode, NimNode) =
+    # Looks for the last statement of the last statement, etc...
+    case n.kind
+    of nnkIfExpr, nnkIfStmt, nnkTryStmt, nnkCaseStmt:
+      result[0] = copyNimTree(n)
+      result[1] = copyNimTree(n)
+      result[2] = copyNimTree(n)
+      for i in ord(n.kind == nnkCaseStmt)..<n.len:
+        (result[0][i], result[1][^1], result[2][^1]) = transLastStmt(n[i], res, bracketExpr)
+    of nnkStmtList, nnkStmtListExpr, nnkBlockStmt, nnkBlockExpr, nnkWhileStmt,
+        nnkForStmt, nnkElifBranch, nnkElse, nnkElifExpr, nnkOfBranch, nnkExceptBranch:
+      result[0] = copyNimTree(n)
+      result[1] = copyNimTree(n)
+      result[2] = copyNimTree(n)
+      if n.len >= 1:
+        (result[0][^1], result[1][^1], result[2][^1]) = transLastStmt(n[^1], res, bracketExpr)
+    of nnkTableConstr:
+      result[1] = n[0][0]
+      result[2] = n[0][1]
+      if bracketExpr.len == 1:
+        bracketExpr.add([newCall(bindSym"typeof", newEmptyNode()), newCall(
+            bindSym"typeof", newEmptyNode())])
+      template adder(res, k, v) = res[k] = v
+      result[0] = getAst(adder(res, n[0][0], n[0][1]))
+    of nnkCurly:
+      result[2] = n[0]
+      if bracketExpr.len == 1:
+        bracketExpr.add(newCall(bindSym"typeof", newEmptyNode()))
+      template adder(res, v) = res.incl(v)
+      result[0] = getAst(adder(res, n[0]))
+    else:
+      result[2] = n
+      if bracketExpr.len == 1:
+        bracketExpr.add(newCall(bindSym"typeof", newEmptyNode()))
+      template adder(res, v) = res.add(v)
+      result[0] = getAst(adder(res, n))
+  
+  macro collect*(init, body: untyped): untyped =
+    runnableExamples:
+      import sets, tables
+      let data = @["bird", "word"]
+      ## seq:
+      let k = collect(newSeq):
+        for i, d in data.pairs:
+          if i mod 2 == 0: d
+      assert k == @["bird"]
+      ## seq with initialSize:
+      let x = collect(newSeqOfCap(4)):
+        for i, d in data.pairs:
+          if i mod 2 == 0: d
+      assert x == @["bird"]
+      ## HashSet:
+      let y = initHashSet.collect:
+        for d in data.items: {d}
+      assert y == data.toHashSet
+      ## Table:
+      let z = collect(initTable(2)):
+        for i, d in data.pairs: {i: d}
+      assert z == {0: "bird", 1: "word"}.toTable
+    
+    let res = genSym(nskVar, "collectResult")
+    expectKind init, {nnkCall, nnkIdent, nnkSym}
+    let bracketExpr = newTree(nnkBracketExpr,
+      if init.kind == nnkCall: init[0] else: init)
+    let (resBody, keyType, valueType) = transLastStmt(body, res, bracketExpr)
+    if bracketExpr.len == 3:
+      bracketExpr[1][1] = keyType
+      bracketExpr[2][1] = valueType
+    else:
+      bracketExpr[1][1] = valueType
+    let call = newTree(nnkCall, bracketExpr)
+    if init.kind == nnkCall:
+      for i in 1 ..< init.len:
+        call.add init[i]
+    result = newTree(nnkStmtListExpr, newVarStmt(res, call), resBody, res)
+  
+  proc input*(): string {.inline.} = stdin.readLine
+  proc inputs*(): seq[string] {.inline.} = stdin.readLine.split
+  proc inputInt*(): int {.inline.} = stdin.readLine.parseInt
+  proc inputInts*(): seq[int] {.inline.} = stdin.readLine.split.map(parseInt)
+  proc chmax*[T](n: var T, m: T) {.inline.} = n = max(n, m)
+  proc chmin*[T](n: var T, m: T) {.inline.} = n = min(n, m)
+  proc `%=`*[T: SomeInteger](n: var T, m: T) {.inline.} = n = floorMod(n, m)
+  proc `|=`*[T: SomeInteger or bool](n: var T, m: T) {.inline.} = n = n or m
+  proc `&=`*[T: SomeInteger or bool](n: var T, m: T) {.inline.} = n = n and m
+  proc `^=`*[T: SomeInteger or bool](n: var T, m: T) {.inline.} = n = n xor m
+  proc `<<=`*[T: SomeInteger](n: var T, m: T) {.inline.} = n = n shl m
+  proc `>>=`*[T: SomeInteger](n: var T, m: T) {.inline.} = n = n shr m
+
+when isMainModule:
+  var N, L: int
+  (N, L) = inputInts()
+  var X, T: array[210, int]
+  for i, xi in inputInts():
+    X[i + 1] = xi
+  for i, ti in inputInts():
+    T[i + 1] = ti
+  X[N + 1] = L
+  
+  const INF = 10^18
+  var dp: array[210, array[210, array[2, array[201, int]]]]
+  for i in 0..N + 1:
+    for j in 0..N + 1:
+      for k in 0..1:
+        for m in 0..N:
+          dp[i][j][k][m] = INF
+  
+  var ans = 0
+  dp[0][N + 1][0][0] = 0
+  dp[0][N + 1][1][0] = 0
+  for left in 0..<N:
+    for right in countdown(N + 1, left + 2):
+      for cnt in 0..<N:
+        var t = min(
+          dp[left][right][0][cnt] + X[left + 1] - X[left],
+          dp[left][right][1][cnt] + L - X[right] + X[left + 1]
+        )
+        var ncnt = cnt
+        if t <= T[left + 1]:
+          ncnt.inc
+          ans.chmax(ncnt)
+        dp[left + 1][right][0][ncnt].chmin(t)
+        
+        t = min(
+          dp[left][right][0][cnt] + L - X[right - 1] + X[left],
+          dp[left][right][1][cnt] + X[right] - X[right - 1]
+        )
+        ncnt = cnt
+        if t <= T[right - 1]:
+          ncnt.inc
+          ans.chmax(ncnt)
+        dp[left][right - 1][1][ncnt].chmin(t)
+  echo ans
